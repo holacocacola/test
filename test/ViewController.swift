f8c3a4fa20e0ef945d361ebf712bf114f31a645f
Practicum <practicum@yandex.ru>
2025-10-26 16:52:58 +0300

    Изменения всякой куйни

---
+++ test/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by d m on 18.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    
+    @IBOutlet weak var valueLabel: UILabel!
+    
+    @IBOutlet weak var buttonSecret: UIButton!
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        
+        let val = sender.value
+        valueLabel.text = String(format: "%.2f", val*100)
+        
+        if val > 0.5 {
+            buttonSecret.isEnabled = true
+        } else {
+            buttonSecret.isEnabled = false
+
+        }
+    
+        
+    }
+    
+    @IBAction func buttonAction(_ sender: Any) {
+        let alert = UIAlertController(
+                title: "Вопрос",
+                message: "Вы уверены, что хотите продолжить?",
+                preferredStyle: .alert
+            )
+            
+            // кнопка "Нет" — просто закрывает окно
+            let noAction = UIAlertAction(title: "Нет", style: .cancel, handler: nil)
+            
+            // кнопка "Да" — выполняет действие
+            let yesAction = UIAlertAction(title: "Да", style: .default) { _ in
+                // Здесь твоё действие
+                self.valueLabel.text = "Вы выбрали Да 👍"
+            }
+            
+            // добавляем обе кнопки
+            alert.addAction(noAction)
+            alert.addAction(yesAction)
+            
+            // показываем окно
+            present(alert, animated: true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ testTests/testTests.swift
@@ -0,0 +1,17 @@
+//
+//  testTests.swift
+//  testTests
+//
+//  Created by d m on 18.10.2025.
+//
+
+import Testing
+@testable import test
+
+struct testTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
